b3b617a8fcb7da5d0d0d3b8ac2fb056cef21aad0
jash90 <bartekziimny90@gmail.com>
2023-05-15 23:29:22 +0200

    feature(user): add env, add authenticateAdmin, setup access

--- index.js
+++ index.js
@@ -6,12 +6,28 @@ const upload = require('./middleware/upload');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpecs = require('./swagger');
 const cors = require('cors');
-const {getRentalsByBoardGame, clearRatingsByBoardGame, addBoardGameReview, getRentalByBoardGameId} = require("./routes/rentals");
-const {importBoardGames, deleteBoardGame, returnBoardGame, borrowBoardGame, updateBoardGame, getBoardGame,
+require('dotenv').config();
+const {
+    getRentalsByBoardGame,
+    clearRatingsByBoardGame,
+    addBoardGameReview,
+    getRentalByBoardGameId
+} = require("./routes/rentals");
+const {
+    importBoardGames, deleteBoardGame, returnBoardGame, borrowBoardGame, updateBoardGame, getBoardGame,
     getBoardGames, createBoardGame
 } = require("./routes/boardGame");
 const {validationBoardGame} = require("./validation/boardgame");
-const {authenticateToken, register, login, getUser, setAdmin, getUsers, updateUserRole} = require("./routes/users");
+const {
+    authenticateToken,
+    register,
+    login,
+    getUser,
+    setAdmin,
+    getUsers,
+    updateUserRole,
+    authenticateAdmin
+} = require("./routes/users");
 const {validateRegistration} = require("./validation/registerUser");
 
 
@@ -19,26 +35,26 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/boardgames', authenticateToken, validationBoardGame, upload.single('image'),createBoardGame);
-app.get("/boardgames",getBoardGames);
-app.get('/boardgames/:id', getBoardGame);
-app.put('/boardgames/:id', authenticateToken,validationBoardGame, upload.single('image'), updateBoardGame);
-app.delete('/boardgames/:id', authenticateToken,deleteBoardGame);
-app.post('/boardgames/borrow', authenticateToken,borrowBoardGame);
-app.post('/boardgames/return', authenticateToken,returnBoardGame);
-app.post("/boardgames/bulk-import", authenticateToken,importBoardGames);
+app.get("/boardgames", authenticateToken, getBoardGames);
+app.get('/boardgames/:id', authenticateToken, getBoardGame);
+app.post('/boardgames', authenticateAdmin, validationBoardGame, upload.single('image'), createBoardGame);
+app.put('/boardgames/:id', authenticateAdmin, validationBoardGame, upload.single('image'), updateBoardGame);
+app.delete('/boardgames/:id', authenticateAdmin, deleteBoardGame);
+app.post('/boardgames/borrow', authenticateToken, borrowBoardGame);
+app.post('/boardgames/return', authenticateToken, returnBoardGame);
+app.post("/boardgames/bulk-import", authenticateAdmin, importBoardGames);
 
-app.get("/rentalByBoardgameId/:boardGameId", authenticateToken,getRentalByBoardGameId);
-app.post('/rentals/:id/review', authenticateToken,addBoardGameReview);
-app.get('/rentals/:gameId', authenticateToken,getRentalsByBoardGame);
-app.delete('/rentals/:gameId/clearRatings', authenticateToken,clearRatingsByBoardGame);
+app.get("/rentalByBoardgameId/:boardGameId", authenticateToken, getRentalByBoardGameId);
+app.post('/rentals/:id/review', authenticateToken, addBoardGameReview);
+app.get('/rentals/:gameId', authenticateToken, getRentalsByBoardGame);
+app.delete('/rentals/:gameId/clearRatings', authenticateAdmin, clearRatingsByBoardGame);
 
 app.post('/register', validateRegistration, register);
 app.post('/login', login)
-app.get("/user", getUser)
-app.post('/set-admin', setAdmin)
-app.get("/users", getUsers)
-app.patch("/users/:id",updateUserRole)
+app.get("/user", authenticateToken, getUser)
+app.post('/set-admin', authenticateAdmin, setAdmin)
+app.get("/users", authenticateAdmin, getUsers)
+app.patch("/users/:id", authenticateAdmin, updateUserRole)
 
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
