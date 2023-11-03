f8b53e780fdbf28f5afb0615c3dbeb26ff0830a6
Bartlomiej Zimny <bartlomiejzimny@odrabiamy.pl>
2023-11-03 03:12:00 +0100

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,13 +10,13 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest'
   },
-  plugins:["unused-imports"],
+  plugins: ['unused-imports'],
   rules: {
-    "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "error",
-    "unused-imports/no-unused-vars": [
-      "warn",
-      { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
     ]
   }
 }
